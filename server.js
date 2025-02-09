854c3b1f7b5da9eefffe446b92ed15be858e1072
yukta ajjampur <yukta.ajjampur@yahoo.com>
2025-02-09 13:28:57 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use('/',express.static('public'));
+app.use(cors());
 const budget = {
     myBudget: [
         {
@@ -26,6 +29,15 @@ app.get('/hello',(req,res) => {
 app.get('/budget',(req,res) => {
     res.json(budget);
 });
+app.get("/new-budget-endpoint", (req, res) => {
+    fs.readFile("budget.json", "utf8", (err, data) => {
+      if (err) {
+        return res.status(500).json({ error: "Failed to read budget data" });
+      }
+      res.json(JSON.parse(data));
+    });
+  });
+  
 
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:${port}');
